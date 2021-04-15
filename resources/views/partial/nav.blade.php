34daff5ebfe41b7997b5cae85dd3383aac7e6685
saliouvj <m.saliou90@gmail.com>
2021-04-15 14:38:54 +0200

    front ok

---
+++ resources/views/partial/nav.blade.php
@@ -0,0 +1,28 @@
+<nav id="primary-navigation" class="site-navigation">
+
+    <div class="container-fluid">
+
+        <div class="navbar-header">
+            
+            <a href={{route("home")}} class="site-title yellow-text ">Business Perfect</a>
+
+        </div><!-- /.navbar-header -->
+
+        <div class="main-menu" id="perfect-navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li class="active">
+                    <a href={{route("home")}}>Home</a></li>
+                <li>
+                    <a href={{route("blog")}}>Blog </a>
+                </li>
+                <li>
+                    <a href={{route("portofolio")}}>Portfolio</a>                                
+                </li>
+                <li>
+                    <a href={{route("contact")}}>Contact</a>
+                </li> 
+            </ul><!-- /.navbar-nav -->
+        </div><!-- /.navbar-collapse -->
+        
+    </div>
+</nav><!-- /.site-navigation -->
